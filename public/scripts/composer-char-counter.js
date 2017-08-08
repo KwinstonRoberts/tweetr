be0ba5cd3afec16f454347ab0bbfe8726a326b4c
kyle roberts <kyle.w.r@hotmail.com>
2017-08-08 02:00:52 +0000

    ficed char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	console.log('loaded');
-	$(".new-tweet").on('input','textarea',function(e){
+	$("#new-tweet").on('input','textarea',function(e){
 
 		var count = 140 - $(this).val().length;
 		var counter = $(this).siblings('.counter');
