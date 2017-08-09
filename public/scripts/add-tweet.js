f21df2ad018865fa8e6a3e0cef7b695fbb8b8133
kyle roberts <kyle.w.r@hotmail.com>
2017-08-09 18:04:52 +0000

    added modified tweet structure, merged likeTweet function into callback on load

--- public/scripts/add-tweet.js
+++ public/scripts/add-tweet.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+  var focused = false;
   $('nav').on('click','button',function(e){
     e.preventDefault();
     $('#new-tweet').slideToggle('fast',function(){
-      
-      $('#new-tweet textarea').focus();
+      focused = !focused;
+      $('#new-tweet').focus();
     });
   });
   $('#new-tweet>form').on('submit', function(e){
@@ -17,20 +18,21 @@ $(document).ready(function(){
     }else{
       $('.invalid').remove();
       $.post('/tweets', $(this).serialize() ,function(){
-        $.get('/tweets', function(d){      
+        $.get('/tweets', function(d){
           $('#tweets').empty();
           var tweetList = "";
           var date = new Date();
           for(x in d){
             console.log(d[x]);
-            $('#tweets').prepend(`<article>
+            $('#tweets').prepend(`
+              <article>
               <header>
-                <img src='${d[x].user.avatars.small}'/>
+                <img src='${d[x].user.avatars.large}'/>
                 <h2>${d[x].user.name}</h2>
                 <p>${d[x].user.handle}</p>
               </header>
               <p>${d[x].content.text}</p>
-              <footer>${Math.floor((date.getTime() - d[x].created_at)/1000/60/60/24/365)} year(s) ago</footer>
+              <footer>${Math.floor((date.getTime() - d[x].created_at)/1000/60/60/24/365)} year(s) ago <b class='hidden invalid'> ${d.liked} likes </b> <button data-liked=true>Like</button></footer>
             </article>`);
           }
           message.val('');
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,26 +4,51 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function(){
-
-
-  function loadTweets(){
+  var loadTweets = function(){
     $.get('/tweets',function(data){
-      console.log('getting tweets',data)
-      renderTweets(data.sort(function(a,b){
-        return b.created_at - a.created_at;
-      }));
+      renderTweets(data, function(){
+          $('#tweets>article>footer>button').on('click', function(e){
+            var hasBeenLiked = $(e.target).text() === 'Like';
+            console.log(hasBeenLiked);
+            e.preventDefault();
+            $.ajax({
+              url: '/tweets',
+              type: 'PUT',
+              data: `liked=${hasBeenLiked}&name=${$(this).closest('article').find('header').find('h2').text()}`,
+              success: function(data) {
+                console.log(data);
+                if($(e.target).data('liked')){
+                  $(e.target).text(`${data.liked} Likes`);
+                  alert(`you liked!`);
+                  $(e.target).data('liked',false);
+                  console.log($(e.target).data('liked'));
+                }else{
+                  $(e.target).text('Like');
+                  alert(`you unliked!`);
+                    $(e.target).data('liked',true);
+                  console.log($(e.target).data('liked'));
+                }
+              },
+              error: function(err){
+              console.log(err)
+            }
+          });
+        });
+      });
     });
+  }();
+
+
+
+  function renderTweets(tweets, cb){
+    for(x in tweets){
+       $('#tweets').append(createTweetElement(tweets[x]));
+    }
+    cb();
   }
 
-  loadTweets();
-
-  function renderTweets(tweets){
-      for(x in tweets){
-         $('#tweets').append(createTweetElement(tweets[x]));
-      }
-    }
-
   function createTweetElement(data){
+    console.log(data)
     var user = data.user;
     var date = new Date();
     var escape = function(str){
@@ -31,17 +56,14 @@ $(document).ready(function(){
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
-    console.log(user);
-    return `<article>
+    return`<article>
               <header>
-                <img src='${escape(user.avatars.small)}'/>
+                <img src='${escape(user.avatars.large)}'/>
                 <h2>${escape(user.name)}</h2>
                 <p>${escape(user.handle)}</p>
               </header>
               <p>${escape(data.content.text)}</p>
-              <footer>${Math.floor((date.getTime() - data.created_at)/1000/60/60/24/365)} year(s) ago</footer>
+              <footer>${Math.floor((date.getTime() - data.created_at)/1000/60/60/24/365)} year(s) ago <button data-liked=true>Like</button></footer>
             </article>`
     }
-  
-
 });
