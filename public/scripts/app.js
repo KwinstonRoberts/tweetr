4eda7d535fb4e81ae67a7d92740081d1d01bb21c
kyle roberts <kyle.w.r@hotmail.com>
2017-08-11 22:06:28 +0000

    fixing issue with database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,29 +42,17 @@
 
       function renderTweets(tweets){
         for(x in tweets){
-           $('#tweets').append(createTweetElement(tweets[x]));
-        }
-      }
-
-      function createTweetElement(d){
-        if(d!== {} ){
-          console.log(d);
-          var user = d.user;
-          var date = new Date();
-          var escape = function(str){
-            var div = document.createElement('div');
-            div.appendChild(document.createTextNode(str));
-            return div.innerHTML;
-          };
-          return`<article>
-                    <header>
-                      <img src='${escape(user.avatars.large)}'/>
-                      <h2>${escape(user.name)}</h2>
-                      <p>${escape(user.handle)}</p>
-                    </header>
-                    <p>${escape(data.content.text)}</p>
-                    <footer>${Math.floor((date.getTime() - data.created_at)/1000/60/60/24)} day(s) ago <i class='fa fa-heart'></i><i class='fa fa-retweet'></i><i class='fa fa-flag'></i></footer>
-                  </article>`
+           $('#tweets').append(`
+                <article>
+                     <header>
+                       <img src='${escape(user.avatars.large)}'/>
+                       <h2>${escape(user.name)}</h2>
+                       <p>${escape(user.handle)}</p>
+                     </header>
+                     <p>${escape(data.content.text)}</p>
+                     <footer>${Math.floor((date.getTime() - data.created_at)/1000/60/60/24)} day(s) ago <i class='fa fa-heart'></i><i class='fa fa-retweet'></i><i class='fa fa-flag'></i></footer>
+                   </article>`);
           }
-        }
-    });
+      }
+    }
+});
