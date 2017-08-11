e448639e70c1ea66a0d2e98beca6680abdab795a
kyle roberts <kyle.w.r@hotmail.com>
2017-08-11 21:29:12 +0000

    merged submission version

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-  $.getScript('/scripts/add-tweet.js', function(){
     $(document).ready(function(){
       var loadTweets = function(){
         $.get('/tweets',function(data){
@@ -13,7 +12,38 @@
         });
       }();
 
-
+      function addTweet(){
+        var focused = false;
+        $('nav').on('click','button',function(e){
+          e.preventDefault();
+          $('#new-tweet').slideToggle('fast',function(){
+            focused = !focused;
+            $('#new-tweet').focus();
+          });
+        });
+        $('#new-tweet>form').on('submit', function(e){
+          e.preventDefault();
+          var message = $('#new-tweet form textarea');
+          console.log(message);
+          if(message.val().length > 140){
+            $('section>h2').after('<p class="invalid"> your text has too many characters</p>');
+          }else if(message.val().length === 0){
+            $('section>h2').after('<p class="invalid"> Your text is empty</p>');
+          }else{
+            $('.invalid').remove();
+            $.post('/tweets', $(this).serialize() ,function(){
+              $.get('/tweets', function(d){
+                $('#tweets').empty();
+                var tweetList = "";
+                var date = new Date();
+                renderTweets(d,function(){
+                  message.val('');
+                });
+              });
+            });
+          };
+        });
+      }
 
       function renderTweets(tweets, callback){
         for(x in tweets){
@@ -42,4 +72,3 @@
                 </article>`
         }
     });
-  });
